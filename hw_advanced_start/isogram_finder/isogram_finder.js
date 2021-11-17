6a060b81d03019d5ad6c56dcd006ae082d20abaa
Heather King <hkingdevelopment@gmail.com>
2021-11-17 19:28:13 +0000

    adds a solution to Isogram Finder

--- hw_advanced_start/isogram_finder/isogram_finder.js
+++ hw_advanced_start/isogram_finder/isogram_finder.js
@@ -1,9 +1,23 @@
 const IsogramFinder = function (word) {
-
+    this.word = word;
 }
 
+// couldn't figure out how to do this without a loop!
 IsogramFinder.prototype.isIsogram = function () {
+    let lower = this.word.toLowerCase();
+    let split = lower.split('');
+    let sortedWord = split.sort();
 
+    let previousLetter = "";
+    let duplicate = true;
+
+    for (var i = 0; i < sortedWord.length; i++) {
+        if (sortedWord[i] === previousLetter) {
+            let duplicate = false;
+            return duplicate
+        }
+        previousLetter = sortedWord[i];
+    } return duplicate;
 }
 
 module.exports = IsogramFinder;
