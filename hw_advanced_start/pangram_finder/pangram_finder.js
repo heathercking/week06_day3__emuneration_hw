b382efb2b717def5292ce6f0ef2b6c8b55bf4f07
Heather King <hkingdevelopment@gmail.com>
2021-11-17 16:46:21 +0000

    adds isPangram method - passes 6 out of 7 tests
    needs amending to be able to detect a pangram with mixed case

--- hw_advanced_start/pangram_finder/pangram_finder.js
+++ hw_advanced_start/pangram_finder/pangram_finder.js
@@ -1,9 +1,17 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase.split('');
 }
 
-PangramFinder.prototype.isPangram = function () {
 
+PangramFinder.prototype.isPangram = function () {
+  const hasEveryLetter = (letter) => this.isInPhrase(letter);
+  return this.alphabet.every(hasEveryLetter);
+}
+
+PangramFinder.prototype.isInPhrase = function (input) {
+  const found = this.phrase.find(letter => letter === input);
+  return found;
 }
 
 module.exports = PangramFinder;
